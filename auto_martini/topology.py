6b74e9736927a4893fcf9e1a0639c6fec806cdbf
Magdalena Szczuka <166702715+szczukam@users.noreply.github.com>
2024-06-14 14:57:52 +0200

    prettier molecule ascii image in itp
    added atom symbols and indices

--- auto_martini/topology.py
+++ auto_martini/topology.py
@@ -36,11 +36,15 @@ from .common import *
 import numpy as np
 import random
 import re
+import numpy as np
+
 from rdkit.Chem import AllChem
 from rdkit import Chem
 from rdkit.Chem import Draw
+from rdkit.Chem.Draw import rdMolDraw2D
+from rdkit.Chem import rdCoordGen
+from rdkit import Geometry
 from PIL import Image
-import numpy as np
 
 logger = logging.getLogger(__name__)
 
@@ -136,37 +140,107 @@ def gen_molecule_sdf(sdf):
             exit(1)
     return molecule
 
-def image_to_ascii(image_path, output_width=30):
-    # Load the image
-    image = Image.open(image_path)
-    # Convert image to grayscale
-    image = image.convert('L')
-    # Resize image based on the output width
-    width, height = image.size
-    aspect_ratio = height / width
-    new_height = int(output_width * aspect_ratio)
-    image = image.resize((output_width, new_height))
-    # Define ASCII characters
-    ascii_chars = "@%#*+=- ."
-    # Convert each pixel to an ASCII character
-    pixels = np.array(image)
-    ascii_str = ";"
-    for row in pixels:
-        line = ""
-        for pixel in row:
-            line += ascii_chars[pixel // 32]
-        if line.strip():  # Add the line only if it is not empty
-            ascii_str += line + "\n;"
-    return ascii_str
-
-def smiles_to_ascii(mol,molname, output_width=30):
-    # Draw the molecule and save as image
-    img = Draw.MolToImage(mol)
-    img_path = str(molname)+'.png'
-    img.save(img_path)
-    # Convert image to ASCII art
-    ascii_art = image_to_ascii(img_path, output_width)
-    return ascii_art
+def mol_to_ascii(mol):
+    """
+    script adapted from rdkit_print_mol_ascii.ipynb by Vincent F. Scalfani (git: vfscalfani)
+    """
+    #1. Draw mol with rdCoordGen
+    rdCoordGen.AddCoords(mol)
+    drawer = rdMolDraw2D.MolDraw2DCairo(300,300)
+    drawer.drawOptions().fixedBondLength = 40
+    drawer.DrawMolecule(mol)
+    drawer.FinishDrawing()
+    ascii_img=""
+    #2. Get atom x,y coordinates
+    # adapated from: https://gist.github.com/greglandrum/0c8b80825826392a6519af7519862baf
+    atom_index = []
+    atom_symbols = []
+    atom_xpos = []
+    atom_ypos = []
+    conf = mol.GetConformer()
+    for i in range(mol.GetNumAtoms()):
+        atom_index.append(i)
+        at_sym=str(mol.GetAtomWithIdx(i).GetSymbol())+str(i)
+        atom_symbols.append(at_sym)
+        pos_A = conf.GetAtomPosition(i)
+        pos_point = Geometry.Point2D(pos_A.x,pos_A.y)
+        dpos = drawer.GetDrawCoords(pos_point)
+        atom_xpos.append(dpos.x)
+        atom_ypos.append(dpos.y)
+    #3. Resize and shift coordinates
+    atom_coords = list(zip(atom_xpos, atom_ypos))
+    # A scale between 1/12 and 1/20 seems to work okay without distorting printing aspect ratio
+    scale_atom_coords = [(p[0]/14, p[1]/14) for p in atom_coords]
+    round_atom_coords = [(round(p[0]), round(p[1])) for p in scale_atom_coords]
+    # Get min and max for x,y
+    xmin = min(c[0] for c in round_atom_coords)
+    xmax = max(c[0] for c in round_atom_coords)
+    ymin = min(c[1] for c in round_atom_coords)
+    ymax = max(c[1] for c in round_atom_coords)
+    # figure out what to substract to shift coordinates by a min
+    if xmin < ymin:
+        xymin = xmin
+    else:
+        xymin = ymin
+    # shift by xymin to move mol closer to origin
+    norm_atom_coords = [(p[0] - xymin, p[1] - xymin) for p in round_atom_coords]
+    #4. Get Bond connection information for drawing bonds
+    atom_begin = []
+    atom_end = []
+    bond_type = []
+    for bond in mol.GetBonds():
+        atom_begin.append(bond.GetBeginAtomIdx())
+        atom_end.append(bond.GetEndAtomIdx())
+        bond_type.append(bond.GetBondTypeAsDouble()) # this could be used to define single, double, arom with symbols
+    #5. Get coordinates of middle of bond for plotting
+    begin_connection_xpos = []
+    begin_connection_ypos = []
+    for i in atom_begin:
+        begin_connection_xpos.append(norm_atom_coords[i][0])
+        begin_connection_ypos.append(norm_atom_coords[i][1])
+    end_connection_xpos = []
+    end_connection_ypos = []
+    for i in atom_end:
+        end_connection_xpos.append(norm_atom_coords[i][0])
+        end_connection_ypos.append(norm_atom_coords[i][1])
+    midx_bond = []
+    for x1,x2 in zip(begin_connection_xpos, end_connection_xpos):
+        middle = (x1+x2)/2
+        midx_bond.append(middle)
+    midy_bond = []
+    for y1,y2 in zip(begin_connection_ypos, end_connection_ypos):
+        middle = (y1+y2)/2
+        midy_bond.append(middle)
+    # xy coordinates of center of bonds (*) to plot
+    bond_center_points = list(zip(midx_bond,midy_bond))
+    #6. Setup a max plotting print range based on coordinates
+    if xmax > ymax:
+        xymax = xmax
+    else:
+        xymax = ymax
+    # this just gets the steps for x.y coordinates in increments of 0.5 for the print grid
+    start = 0
+    stop = round(xymax+1)
+    samples = round((stop*2)+1)
+    print_range = np.linspace(start,stop,samples)
+    #7. Print the molecule!
+    # Adapted from: https://stackoverflow.com/questions/16240358/ascii-graph-in-python // https://creativecommons.org/licenses/by-sa/3.0/
+    bond_symbol = '*'
+    fill_symbol = ' '
+    for y in print_range:
+        chars = []
+        for x in print_range:
+            if (x,y) in norm_atom_coords:
+                index_value = norm_atom_coords.index((x,y))
+                chars.append(atom_symbols[index_value])
+            elif (x,y) in bond_center_points:
+                chars.append(bond_symbol)
+            else:
+                chars.append(fill_symbol)
+        line=(' '.join(chars))
+        if line.strip():
+            ascii_img +=";  "+ line + '\n'
+    return ascii_img
 
 def print_header(molname, mol_smi):
     """Print topology header"""
@@ -174,7 +248,7 @@ def print_header(molname, mol_smi):
     ascii_art = ""
     mol = Chem.MolFromSmiles(mol_smi)
     if mol is not None:
-        ascii_art = smiles_to_ascii(mol,molname)
+        ascii_art = mol_to_ascii(mol)
     info = (
         "; Developed by: Kiran Kanekal, Tristan Bereau, and Andrew Abi-Mansour\n"
         + "; updated to Martini3 by Magdalena Szczuka, reviewed by Matthieu Chavent \n"
