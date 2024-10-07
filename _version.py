e64ec6a667402e44c6548a5ee7806b2b749866f3
Magdalena Szczuka <166702715+szczukam@users.noreply.github.com>
2024-10-07 16:26:20 +0200

    Revert "working version of the code "
    This reverts commit 3240f34a65ad8fb97ce6c525572bdc61e9039e1b.

--- _version.py
+++
@@ -1,7 +0,0 @@
-try:
-    from importlib import metadata
-except ImportError:  # pragma: no cover
-    # For Py37 and below, use the import_metadata backport
-    import importlib_metadata as metadata
-
-__version__ = metadata.version("auto_martini")
